be11282fb8c10f504330bfbf44b3c95b1bd11628
nativenewengland <maxeconte@gmail.com>
2025-10-29 04:29:10 -0700

    Add moonwell ambient structure for elf forests

--- src/assets.js
+++ src/assets.js
@@ -578,6 +578,7 @@ export const baseTileCoords = {
   OASIS: { row: 0, col: 12 },
   HAMLET_SNOW: { row: 0, col: 13 },
   AMBIENT_SLEEPING_DRAGON: { row: 0, col: 14 },
+  AMBIENT_MOONWELL: { row: 6, col: 2 },
   AMBIENT_GREAT_TREE: { row: 1, col: 14 },
   LIZARDMEN_CITY: { row: 2, col: 11 },
   SAINT_SHRINE: { row: 1, col: 11 },
