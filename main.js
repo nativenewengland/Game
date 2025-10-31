e6c36701ef23dc5b9a9b35ea5c4f7fa1852d474d
nativenewengland <maxeconte@gmail.com>
2025-10-30 21:27:20 -0700

    Revert "Load start screen interactions in entry module"

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
 import { getElements, getMusicToggleElements, getMusicVolumeInputs, getMusicNowPlayingDisplays } from './src/ui/elements.js';
-import './start-game.js';
-
 const elements = getElements();
 
