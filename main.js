1252d42c683a660066ccaee32aa15e6b3e7579b5
nativenewengland <maxeconte@gmail.com>
2025-11-03 05:42:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7777,7 +7777,14 @@ function applyCulturalInfluence({
     { key: 'half_orcs', label: 'Half-Orcs', biomes: ['grassland', 'badlands', 'desert'] },
     { key: 'half_elves', label: 'Half-Elves', biomes: ['forest', 'grassland'] },
     { key: 'dryad', label: 'Dryad', biomes: ['forest', 'marsh', 'lake'] },
-    { key: 'leshy', label: 'Leshy', biomes: ['forest', 'tundra', 'marsh'] }
+    { key: 'leshy', label: 'Leshy', biomes: ['forest', 'marsh'] },
+    { key: 'satyr', label: 'Satyr', biomes: ['forest', 'grassland'] },
+    { key: 'hobgoblin', label: 'Hobgoblin', biomes: ['badlands', 'mountain', 'grassland'] },
+    { key: 'locathah', label: 'Locathah', biomes: ['ocean', 'lake', 'marsh'] },
+    { key: 'firbolg', label: 'Firbolg', biomes: ['forest', 'grassland'] },
+    { key: 'aarakocra', label: 'Aarakocra', biomes: ['mountain', 'grassland'] },
+    { key: 'braxat', label: 'Braxat', biomes: ['desert', 'badlands', 'jungle'] },
+    { key: 'hadozee', label: 'Hadozee', biomes: ['ocean', 'jungle'] }
   ];
 
   const defaultMinorFolkThreshold = 0.0011;
