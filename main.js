8584d1cb0d3da25797b7ae33343688ea149b96ad
nativenewengland <maxeconte@gmail.com>
2026-01-03 12:18:35 -0500

    Revert "Hide 'More Info' action in structure context menu when details are missing"

--- main.js
+++ main.js
@@ -13834,11 +13834,9 @@ function updateStructureContextMenuActions(tile) {
     tile && (tile.structureName || (tile.structureDetails && tile.structureDetails.name))
   );
   if (hasStructureDetails) {
-    moreInfoButton.classList.remove('hidden');
     moreInfoButton.disabled = false;
     moreInfoButton.setAttribute('aria-disabled', 'false');
   } else {
-    moreInfoButton.classList.add('hidden');
     moreInfoButton.disabled = true;
     moreInfoButton.setAttribute('aria-disabled', 'true');
   }
@@ -30831,6 +30829,7 @@ function startApplicationWhenReady() {
 }
 
 startApplicationWhenReady();
+
 
 
 
